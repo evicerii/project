740d08575addd91cbd0c4de2b1c1b0c6287421df
Белченок <kirill.nikula@mail.ru>
2022-06-03 11:53:08 +0300

    master

---
+++ waxom/scripts/main.js
@@ -0,0 +1,26 @@
+Vue.createApp({
+    data() {
+        return {
+            src:["img/main/firstSlide.jpg","img/main/firstSlide.jpg","img/main/firstSlide.jpg","img/main/firstSlide.jpg","img/main/firstSlide.jpg"]
+        }
+    }
+}).mount('#slideListMain')
+
+Vue.createApp({
+    data() {
+        return {
+        }
+    },
+    renderTracked(){
+    let indicatorListMain = document.getElementById('indicatorListMain');
+    let firstSlide = indicatorListMain.querySelector('button');
+    firstSlide.classList.toggle("active")
+    firstSlide.setAttribute("aria-current","true");
+
+    
+    let slide = document.getElementById('slideListMain');
+    let item = slide.querySelector('.carousel-item');
+    item.classList.toggle('active');
+    }
+  }).mount('#indicatorListMain')
+  
